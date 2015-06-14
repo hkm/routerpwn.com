b35df7072d7840694ab69818c72a729bad01a4fb
hkm <pjoaquin@gmail.com>
2015-06-14 12:37:02 -0500

    Added google contact form

--- Contact/index.php
+++ Contact/index.php
@@ -33,7 +33,7 @@
 <tr><td>
 
 <table width="100%" >
-<iframe src="https://docs.google.com/forms/d/179KsFTpogFIl95Px3vG5BXflUqiW90BtA_1tjlftH3c/viewform?embedded=true" width="100%" height="700" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+<iframe src="https://docs.google.com/forms/d/179KsFTpogFIl95Px3vG5BXflUqiW90BtA_1tjlftH3c/viewform?embedded=true" width="760" height="500" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
 </table>
 <script>
 $(document).ready(function() {
